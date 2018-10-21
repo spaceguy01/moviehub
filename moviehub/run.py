a85cc995833c4e1314a177f5d1727a742f42a951
spaceguy01 <joonyi@gmail.com>
2018-10-20 20:34:09 -0700

    modified run.py

--- moviehub/run.py
+++ moviehub/run.py
@@ -1,11 +1,14 @@
 from app import create_app,db
 from app.auth.models import User
-
+from sqlalchemy import exc
 
 flask_app = create_app('prod')
 
 with flask_app.app_context():
     db.create_all()
-    if not User.query.filter_by(user_name='joon').first():
-        User.create_user(user='joon',email='joonyi@gmail.com',password='secret')
-        flask_app.run()
+    try:
+        if not User.query.filter_by(user_name='joon').first():
+            User.create_user(user='joon',email='joonyi@gmail.com',password='secret')
+
+    except IntegrityError:
+            flask_app.run()
